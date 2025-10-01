356b43d6bb2e0f14af1a53ccdba84b4be61aa5c2
Mukul Sharma <101670371+Mukulsh26@users.noreply.github.com>
2025-10-01 17:42:25 +0530

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -75,7 +75,7 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
                 <h1>{`Hi! I'm Mukul, A`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Full Stack Developer", "Web Designer", ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Highly motivated software developer with {experience} years of experience, currently working at MNC. I possess strong skills in both frontend and backend development, with expertise in React.js, Next.js, HTML, CSS, JavaScript, Bootstrap, Node.js, and MySQL.</p>
+                  <p>Highly motivated software developer with {experience} years of experience, currently working at MNC. I possess strong skills in both frontend and backend development, with expertise in React.js, Next.js, HTML, CSS, JavaScript, Bootstrap, Node.js, Java, SpringBoot and MySQL.</p>
                   <button to onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
